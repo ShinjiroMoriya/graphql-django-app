a1c776df064166be502c0a40e917a6b1ed008215
ShinjiroMoriya <>
2018-06-12 14:53:14 +0900

    make it better

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,28 +1,46 @@
-var http = require('http'),
-    httpProxy = require('http-proxy');
+//app.use('/', proxy('http://localhost:' + DJANGO_PORT));
+//
+//app.listen(NODE_PORT);
+
+var express = require('express');
+var proxy = require('http-proxy-middleware');
 
 var NODE_PORT = 4711;
 
-var proxy = httpProxy.createProxyServer({});
+var app = express();
 
-http.createServer(function(req, res) {
-    // For all URLs beginning with /channel proxy to the Node.JS app, for all other URLs proxy to the Django app running on DJANGO_PORT
-    if(req.url.indexOf('/render') === 0) {
-        // Depending on your application structure you can proxy to a node application running on another port, or serve content directly here
-        proxy.web(req, res, { target: 'http://localhost:' + NODE_PORT });
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-        // Proxy WebSocket requests if needed
-        proxy.on('upgrade', function(req, socket, head) {
-            proxy.ws(req, socket, head, { target: 'ws://localhost:' + NODE_PORT });
-        });
-    } else {
-        proxy.web(req, res, { target: 'http://localhost:' + process.env.DJANGO_PORT });
-    }
-}).listen(process.env.PORT);
+app.use('/graphql', proxy('http://127.0.0.1:'+ process.env.DJANGO_PORT));
+app.listen(NODE_PORT);
 
-// Example node application running on another port
-http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('Request successfully proxied to Node.JS app');
-    res.end();
-}).listen(NODE_PORT);
+//var http = require('http'),
+//    httpProxy = require('http-proxy');
+//
+//var NODE_PORT = 4711;
+//
+//var proxy = httpProxy.createProxyServer({});
+//
+//http.createServer(function(req, res) {
+//    console.log('foo')
+//    if(req.url.indexOf('/render') === 0) {
+//        console.log('hoge')
+//        proxy.web(req, res, { target: 'http://localhost:' + NODE_PORT });
+//
+//        // Proxy WebSocket requests if needed
+//        proxy.on('upgrade', function(req, socket, head) {
+//            proxy.ws(req, socket, head, { target: 'ws://localhost:' + NODE_PORT });
+//        });
+//    } else {
+//        proxy.web(req, res, { target: 'http://localhost:' + process.env.DJANGO_PORT });
+//    }
+//}).listen(process.env.PORT);
+//
+//// Example node application running on another port
+//http.createServer(function(req, res) {
+//    res.writeHead(200, { 'Content-Type': 'text/plain' });
+//    res.write('Request successfully proxied to Node.JS app');
+//    res.end();
+//}).listen(NODE_PORT);
